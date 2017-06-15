732a5ae94f155732219838bd6aaa27f59ff57716
nodexpertsdev <development@nodexperts.com>
2017-06-15 11:42:50 +0530

    Fixed AddForm UI

--- step-2/imports/ui/components/add-form.jsx
+++ step-2/imports/ui/components/add-form.jsx
@@ -20,7 +20,7 @@ export default class AddForm extends React.Component {
                      name="name"
                      placeholder="Enter Name" required />
             </div>
-            <div class="form-group col-lg-4">
+            <div className="form-group col-lg-4">
       			    <label for="dob">Date Of Birth</label>
       			    <input type="date"
                        className="form-control"
@@ -28,7 +28,7 @@ export default class AddForm extends React.Component {
                        name="dob"
                        placeholder="Enter DOB" required />
       			</div>
-      			<div class="form-group col-lg-4">
+      			<div className="form-group col-lg-4">
       			    <label for="height">Height(cm)</label>
       			    <input type="number"
                        className="form-control"
@@ -38,41 +38,41 @@ export default class AddForm extends React.Component {
       			</div>
           </div>
 
-          <div class="row">
-      			<div class="form-group col-lg-4">
+          <div className="row">
+      			<div className="form-group col-lg-4">
       			    <label for="iplTeam">IPL Team</label>
-      			    <input type="text" class="form-control"
+      			    <input type="text" className="form-control"
                        id="iplTeam" name="iplTeam"  placeholder="Enter IPL Team" required />
       			</div>
-      			<div class="form-group col-lg-4">
+      			<div className="form-group col-lg-4">
       			    <label for="seasonsPlayed">Seasons Played</label>
-      			    <input type="number" class="form-control" id="seasonsPlayed" name="seasonsPlayed"
+      			    <input type="number" className="form-control" id="seasonsPlayed" name="seasonsPlayed"
                        placeholder="Enter Seasons Played" required />
       			</div>
-      		    <div class="form-group col-lg-4">
+      		    <div className="form-group col-lg-4">
       			    <label for="numOf50s">Number of 50s</label>
-      			    <input type="number" class="form-control" id="numOf50s" name="numOf50s"
+      			    <input type="number" className="form-control" id="numOf50s" name="numOf50s"
                        placeholder="Enter Number of 50s" required />
       			</div>
       		</div>
-      		<div class="row">
-      			<div class="form-group col-lg-4">
+      		<div className="row">
+      			<div className="form-group col-lg-4">
       			    <label for="numOf100s">No of 100s</label>
-      			    <input type="text" class="form-control" id="numOf100s" name="numOf100s"
+      			    <input type="text" className="form-control" id="numOf100s" name="numOf100s"
                        placeholder="Enter No of 100s" required />
       			</div>
-      			<div class="form-group col-lg-4">
+      			<div className="form-group col-lg-4">
       			    <label for="totalBalls">Total Balls Played</label>
-      			    <input type="number" class="form-control" id="totalBalls" name="totalBalls"
+      			    <input type="number" className="form-control" id="totalBalls" name="totalBalls"
                        placeholder="Enter Total Balls Played" required />
       			</div>
-      		    <div class="form-group col-lg-4">
+      		    <div className="form-group col-lg-4">
       			    <label for="totalRuns">Total Runs Scored</label>
-      			    <input type="number" class="form-control" id="totalRuns" name="totalRuns"
+      			    <input type="number" className="form-control" id="totalRuns" name="totalRuns"
                        placeholder="Enter Total Runs Scored" required />
       			</div>
       		</div>
-      		<div class="form-group col-lg-4 col-lg-offset-4">
+      		<div className="form-group col-lg-4 col-lg-offset-4">
       		  <input type="submit" className="form-control btn-primary" id="submit" value="Add Player" />
           </div>
         </form>
